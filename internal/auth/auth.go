355ae011ad94cf3f3325ceff1b5d7241e46864d2
donaldnguyen99 <donaldnguyen99@users.noreply.github.com>
2025-06-24 15:32:02 -0700

    add json web token creator and validator functions

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,4 +22,53 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(hash, password string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	nowUTC := time.Now().UTC()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		Subject: userID.String(),
+		ExpiresAt: jwt.NewNumericDate(nowUTC.Add(expiresIn)),
+		IssuedAt: jwt.NewNumericDate(nowUTC),
+	})
+
+	signedJWT, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	 return signedJWT, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+
+	uuidString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	uuidRes, err := uuid.Parse(uuidString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return uuidRes, nil
+}
+
+func GetBearerToken(headers http.Header) (string, error) {
+	authHeader := headers.Get("Authorization")
+	tokenString, found := strings.CutPrefix(authHeader, "Bearer ")
+	if !found {
+		return "", fmt.Errorf("error: missing 'Bearer ' prefix")
+	}
+
+	return strings.TrimSpace(tokenString), nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,7 +2,11 @@ package auth
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestHashPassword(t *testing.T) {
@@ -32,6 +36,87 @@ func TestHashPassword(t *testing.T) {
 			}
 			if err = CheckPasswordHash(hpw, c.pw); err != nil && c.ok {
 				t.Errorf("expected password '%s' to succeed check", c.pw)
+			}
+		})
+	}
+}
+
+func TestMakeAndValidateJWT(t *testing.T) {
+	cases := []struct {
+		uuid string
+		tokenSecret string
+	}{
+		{
+			uuid: "34277abf-5023-11f0-9fc4-00155d7029f7",
+			tokenSecret: "hello",
+		},
+		{
+			uuid: "34277eec-5023-11f0-9fc4-00155d7029f7",
+			tokenSecret: "hello",
+		},
+		{
+			uuid: "34278025-5023-11f0-9fc4-00155d7029f7",
+			tokenSecret: "hello",
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+			jwt, err := MakeJWT(uuid.MustParse(c.uuid), c.tokenSecret, time.Hour)
+			if err != nil {
+				t.Errorf("expected no error making jwt: %v", err)
+			}
+			if len(jwt) != 208 {
+				t.Errorf("expected jwt of length 208: got %v", len(jwt))
+			}
+			
+			resUUID, err := ValidateJWT(jwt, c.tokenSecret)
+			if err != nil {
+				t.Errorf("expected no error validating jwt: %v", err)
+			}
+			if resUUID.String() != c.uuid {
+				t.Errorf("expected returned uuid of %s: got %s", c.uuid, resUUID)
+			}
+		})
+	}
+}
+
+func TestGetBearerToken(t *testing.T) {
+	cases := []struct{
+		header http.Header
+		expected string
+	}{
+		{
+			header: http.Header{
+				"Authorization": []string{"Bearer 34277abf-5023-11f0-9fc4-00155d7029f7",},
+			},
+			expected: "34277abf-5023-11f0-9fc4-00155d7029f7",
+		},
+		{
+			header: http.Header{
+				"Authorization": []string{"Bearer 34277eec-5023-11f0-9fc4-00155d7029f7",},
+			},
+			expected: "34277eec-5023-11f0-9fc4-00155d7029f7",
+		},
+		{
+			header: http.Header{
+				"Authorization": []string{"34278025-5023-11f0-9fc4-00155d7029f7",},
+			},
+			expected: "error: missing 'Bearer ' prefix",
+		},
+	}
+
+	for i, c := range cases {
+		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
+			res, err := GetBearerToken(c.header)
+			if err != nil {
+				if err.Error() != c.expected {
+					t.Errorf("expected the error: '%s' or no error", c.expected)
+				}
+			} else {
+				if res != c.expected {
+					t.Errorf("expected %s, got %s", c.expected, res)
+				}
 			}
 		})
 	}
